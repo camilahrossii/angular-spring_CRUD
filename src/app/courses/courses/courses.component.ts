b5b945522863de837b43e03d607ef2f803c68dfc
Camila Rossi <camilarossi.contact@gmail.com>
2023-10-24 07:57:56 -0300

    http: get and rxjs

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -8,12 +10,15 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent {
 
-  courses: Course[] = [
-    {_id: '1', name:'Angular', category:'front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  // coursesService:CoursesService;
+
+  constructor( private coursesService:CoursesService ) {
+    // this.courses = [];
+    // this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
   }
 
 }
