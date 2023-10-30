e39b06164f0575ab17ec1fab1cacb03a930bc406
Camila Rossi <camilarossi.contact@gmail.com>
2023-10-30 15:58:57 -0300

    api http get

---
+++ crud-spring/src/main/java/com/crud/model/Course.java
@@ -0,0 +1,27 @@
+package com.crud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//@Table(name = "cursos")
+//@Data // getters, setters, constructor
+
+@Entity
+@Data
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+
+}
